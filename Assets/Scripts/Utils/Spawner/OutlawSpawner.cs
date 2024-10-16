f061094bf506494ea604d2146e43a2d6ed513017
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-10-16 15:47:04 +0900

    Fix OutlawSpawner
    오브젝트 위치 변경, 적절한 변수 도입후 생성간격을 랜덤으로 설정

--- Assets/Scripts/Utils/Spawner/OutlawSpawner.cs
+++ Assets/Scripts/Utils/Spawner/OutlawSpawner.cs
@@ -8,17 +8,32 @@ public class OutlawSpawner : MonoBehaviour
 
     private float spawnX;
     private float spawnY;
-    private float spawnDelay = 5f;
-    private float spawnTimer = 0f;
 
+    private float spawnDelay;
+    private float spawnDelayMin;
+    private float spawnDelayMax;
 
+    private float spawnTimer;
+
+    private float spawnYValue;
 
     private void Awake()
     {
         outlawPool = GetComponent<ObjectPool>();
     }
 
-    private void FixedUpdate()
+    private void Start()
+    {
+        spawnDelay = 3f;
+        spawnDelayMin = 2f;
+        spawnDelayMax = 5f;
+
+        spawnTimer = 0f;
+
+        spawnYValue = 4.5f;
+    }
+
+    private void Update()
     {
         SpawnTimeCheck();
     }
@@ -31,13 +46,14 @@ public class OutlawSpawner : MonoBehaviour
         {
             CreateOutlaw();
             spawnTimer = 0f;
+            spawnDelay = Random.Range(spawnDelayMin, spawnDelayMax);
         }
     }
 
     private void CreateOutlaw()
     {
-        float spawnX = (transform.position.x + 0.5f);
-        float spawnY = Random.Range(-4f, 4f);
+        float spawnX = transform.position.x;
+        float spawnY = Random.Range(-spawnYValue, spawnYValue);
         GameObject obj = outlawPool.SpawningPool("Outlaw");
 
         obj.transform.position = new Vector2(spawnX, spawnY);
