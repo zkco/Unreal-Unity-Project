4f3f69ca07475b172f37ece066dc8d7c9fc673e3
zkco <qkrentks456@gmail.com>
2024-10-17 20:58:54 +0900

    [Update] ObjectSpawner

--- Assets/Scripts/Entities/Barricade/ObjectDefaultStatHandler.cs
+++ Assets/Scripts/Entities/Barricade/ObjectDefaultStatHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ObjectDefaultBehavior : MonoBehaviour
+public class ObjectDefaultStatHandler: MonoBehaviour
 {
     [SerializeField] private ObjectDefaultStat baseStat;
     public ObjectDefaultStat currentStat { get; private set; }
